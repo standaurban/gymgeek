8f3133b588de92f3511333b8db1494736605d3a7
seminar <standa.urban55@gmail.com>
2017-04-05 16:10:57 +0200

    Úlohy

--- uloha2_turtle.py
+++ uloha2_turtle.py
@@ -1,12 +1,16 @@
 import turtle
+
 turtle.shape("turtle")
 turtle.colormode(255)
 turtle.color(102, 153, 153)
-delsi = 300
+
+delsi = 100
 kratsi = delsi / 10
+
 turtle.penup()
 turtle.goto(-delsi/2, delsi/10 * 10)
 turtle.pendown()
+
 for i in range(10):
     turtle.forward(delsi)
     turtle.right(90)
@@ -16,6 +20,7 @@ for i in range(10):
     turtle.left(90)
     turtle.forward(kratsi)
     turtle.left(90)
-turtle.forward(delsi)    
+
+turtle.forward(delsi)
     
 
