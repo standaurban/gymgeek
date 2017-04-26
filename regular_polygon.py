ac269dfcea96c243d8a5526a8ca71338ee1a7bba
Bruce Wayne <standa.urban55@gmail.com>
2017-04-26 15:41:49 +0200

    regular_polygon nyní jako funkce

--- regular_polygon.py
+++ regular_polygon.py
@@ -1,12 +1,15 @@
 import turtle
 
+def regular_polygon(delka, n):
+    for i in range(n):
+        turtle.forward(delka)
+        turtle.left(((2/n)*180))
+        
 n = int(input("Zadej celé a přirozené číslo: "))
 if n >= 2:
-    for i in range(n):
-        turtle.forward(50)
-        turtle.left(((2/n)*180))
+    regular_polygon(80, n)    
 else:
     exit()
 
-#úprava aby jsem mohl změnit komentář na githubu
+
 
